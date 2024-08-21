3e42e1bd0451dd36193b256183f16b7b34348e95
igovi <igovi9910@gmail.com>
2024-08-21 17:36:24 -0300

    Limpando pagina categoria

--- src/app/pages/categoria/categoria.page.ts
+++ src/app/pages/categoria/categoria.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { InfiniteScrollCustomEvent } from '@ionic/angular';
 import { catchError, of, Subscription } from 'rxjs';
 import { Category } from 'src/app/models/category.model';
@@ -11,83 +11,11 @@ import { DataService } from 'src/app/services/storage/data.service';
   templateUrl: 'categoria.page.html',
   styleUrls: ['categoria.page.scss']
 })
-export class CategoriaPage  implements OnInit{
+export class CategoriaPage  {
 
-  public categoryList:Category[] =[];
-
-  total:number = 0;
-  skip:number = 0;
-  take:number = 3;
-
-  public isOnline: boolean = false;
-
-  private networkStatusSubscription: Subscription;
   
-  constructor(private categoryProvider: CategoryProvider,private networkService: NetworkService, private dataService: DataService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.networkCheck();
-  }
   
-  loadCategories() {
-    this.categoryProvider.get(this.skip,this.take).pipe(
-      catchError((apiError: any) => {
-        console.log('Ocorreu um erro: ', apiError);
-        return of([]);
-      })
-    ).subscribe({
-      next: (apiData: any) => {
-        this.categoryList = apiData.Items; 
-        this.total= apiData.Total
-        this.skip = this.take
-        this.dataService.saveData('categoryList', this.categoryList);
-
-      },
-      error: (apiError: any) => {
-        console.log('Unhandled error:', apiError);
-      }
-    });
-  }
-
-  loadStoredCategories(){
-    this.dataService.getData('categoryList').then(storedCategoryList => {
-      if (storedCategoryList) {
-        this.categoryList = storedCategoryList;
-      }
-    });
-  }
-
-  networkCheck(){
-    this.networkStatusSubscription = this.networkService.networkStatus$.subscribe(isOnline => {
-      this.isOnline = isOnline;
-      console.log('Network status updated:', this.isOnline);
-      if (this.isOnline) {
-        this.loadCategories();
-      } else {
-        this.loadStoredCategories();
-      }
-    });
-  }
-
-  loadMore(event: InfiniteScrollCustomEvent): void {
-    this.categoryProvider.get(this.skip, this.take).pipe(
-      catchError((apiError: any) => {
-        console.log('Ocorreu um erro: ', apiError);
-        return of([]);
-      })
-    ).subscribe({
-      next: (apiData: any) => {
-        this.categoryList = [...this.categoryList, ...apiData.Items];
-        this.skip += this.take;
-        this.dataService.saveData('categoryList', this.categoryList);
-        if (this.categoryList.length === this.total) {
-          event.target.disabled = true;
-        }
-        event.target.complete();
-      },
-      error: (apiError: any) => {
-        console.log('Unhandled error:', apiError);
-      }
-    });
-  }
+  
 }
